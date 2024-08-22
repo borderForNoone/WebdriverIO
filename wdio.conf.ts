cae92f24a0d4c06b27cf00391ac9e4c49175b4bb
DenysKulikov <deniska2002kulikov@gmail.com>
2024-08-22 15:21:59 +0300

    changed yml

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -60,7 +60,8 @@ export const config: Options.Testrunner = {
     //
     capabilities: [{
         // capabilities for local browser web tests
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        browserName: 'chrome', // or "firefox", "microsoftedge", "safari"
+        
     }],
 
     //
